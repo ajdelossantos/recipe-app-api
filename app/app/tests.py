c1b33ba1fc302160625c0fa0359df7ea53a2edfc
Alvin James T. Delos Santos <a.jamesdelossantos@gmail.com>
2023-09-06 13:57:24 -0500

    chore: fix typos and resync code with source

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,7 +1,8 @@
 """
-Sample Tests
+Sample tests
 """
 from django.test import SimpleTestCase
+
 from app import calc
 
 
@@ -15,7 +16,7 @@ class CalcTests(SimpleTestCase):
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """Test subtracting numbers together."""
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
